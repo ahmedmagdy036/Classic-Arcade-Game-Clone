985f120fd08120f78df8e9bab18a0f9bc21727ee
ahmedmagdy036 <36425010+ahmedmagdy036@users.noreply.github.com>
2018-02-18 01:34:14 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
-// Enemies 
-var Enemy = function(x,y,speed) {
-	"use strict";
-	this.x=x;
-	this.y=y;
-	this.speed = Math.floor((Math.random()*100)+50);; //function bulit in javascript
+// Enemies our player must avoid
+var Enemy = function(x, y, speed) {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+    this.x = x;
+    this.y = y;
+    this.speed = Math.random()*500;//random value of speed
+
+    
     this.sprite = 'images/enemy-bug.png';
 };
 
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-	this.x +=this.speed*dt;
-	if(this.x >= 505){//xis width in canvas of the secrean
-			this.x=0;
-		}
+    this.x = this.x + this.speed * dt;
+
+    // make enemies loop to left side of canvas after reaching canvas.width
+     if (this.x > 550) {
+        this.x = -100;
+        this.speed = 100 + Math.floor(Math.random() * 512);
+    }
+
+    // Check for collision with enemies 
+    if (player.x < this.x + 60 && player.x + 37 > this.x &&player.y < this.y + 25 &&30 + player.y > this.y) {
+        player.x = 200;
+        player.y = 380;
+    }
+
 };
 
 // Draw the enemy on the screen, required method for game
@@ -19,43 +35,77 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-//player 
-var player = function (x,y,speed){
-	this.x = x;
-	this.y = y;
-	this.speed = speed;
-	this.sprite = 'images/char-horn-girl.png';
+
+var Player = function(x, y, speed) {
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+    this.sprite = 'images/char-boy.png';
+};
+
+Player.prototype.update = function() {
+	if (this.y > 380) {
+        this.y = 380;
+    }
+
+    if (this.x > 400) {
+        this.x = 400;
+    }
+
+    if (this.x < 0) {
+        this.x = 0;
+    }
+
+    // Check for player reaching top of canvas and winning the game
+    if (this.y < 0) {
+        this.x = 200;
+        this.y = 380;
+    }
+   
 }
 
-//update of player
-player.prototype.update = function(dt){
+// Draw the player on the screen, required method for game
 
-}
-//render function
-player.prototype.render = function(){ 
-	ctx.drawImage(Resources.get(this.sprite), this.x, this.y);}
-//handlerinput function
-player.prototype.handlerInput = function(onkeypress){
-	if(onkeypress == 'left')
-		{player.x = player.x - player.speed;}
-	if(onkeypress == 'up')
-		{player.y = player.y-player.speed -30;}
-	if(onkeypress == 'right')
-		{player.x = player.x + player.speed;}
-	if(onkeypress == 'down')
-		{player.x = player.x +player.speed -30;}
-	
-}
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+};
 
+Player.prototype.handleInput = function(keyPress) {
+    if (keyPress == 'left') {
+        player.x -= player.speed;
+    }
+    if (keyPress == 'up') {
+        player.y -= player.speed - 20;
+    }
+    if (keyPress == 'right') {
+        player.x += player.speed;
+    }
+    if (keyPress == 'down') {
+        player.y += player.speed - 20;
+    }
+};
+
+
+ 
 var allEnemies = [];
-for(var i=0; i<4; i++)
-{var enemy = allEnemies.push(new Enemy);
-}
+var score = 0;
+var gameLevel = 1;
+var scoreLevelDiv = document.createElement('div');
+var enemy = new Enemy(0, Math.random() * 184 + 50, Math.random() * 256);
+allEnemies.push(enemy);
+var player = new Player(200, 300, 50);
 
+
+var increaseEnem = function() {
+    
+    allEnemies.length = 0; // clear the canvans
+    for (var i = 0; i <= 4; i++) {
+        var enemy = new Enemy(0, Math.random() * 184 + 50, Math.random() * 256);
+        allEnemies.push(enemy);
+    }
+};
+increaseEnem ();
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
